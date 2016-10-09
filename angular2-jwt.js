57921844061184e891737e20c1b51518dbfc6763
Jesse White <jesse@opennms.org>
2016-10-09 08:26:19 -0400

    Try embedding js-base64 instead.

--- angular2-jwt.js
+++ angular2-jwt.js
@@ -184,8 +184,45 @@ var JwtHelper = (function () {
                 throw 'Illegal base64url string!';
             }
         }
+        var buffer;
+        var fromCharCode = String.fromCharCode;
+        // decoder stuff
+        var re_btou = new RegExp([
+            '[\xC0-\xDF][\x80-\xBF]',
+            '[\xE0-\xEF][\x80-\xBF]{2}',
+            '[\xF0-\xF7][\x80-\xBF]{3}'
+        ].join('|'), 'g');
+        var cb_btou = function (cccc) {
+            switch (cccc.length) {
+                case 4:
+                    var cp = ((0x07 & cccc.charCodeAt(0)) << 18)
+                        | ((0x3f & cccc.charCodeAt(1)) << 12)
+                        | ((0x3f & cccc.charCodeAt(2)) << 6)
+                        | (0x3f & cccc.charCodeAt(3)), offset = cp - 0x10000;
+                    return (fromCharCode((offset >>> 10) + 0xD800)
+                        + fromCharCode((offset & 0x3FF) + 0xDC00));
+                case 3:
+                    return fromCharCode(((0x0f & cccc.charCodeAt(0)) << 12)
+                        | ((0x3f & cccc.charCodeAt(1)) << 6)
+                        | (0x3f & cccc.charCodeAt(2)));
+                default:
+                    return fromCharCode(((0x1f & cccc.charCodeAt(0)) << 6)
+                        | (0x3f & cccc.charCodeAt(1)));
+            }
+        };
+        var btou = function (b) {
+            return b.replace(re_btou, cb_btou);
+        };
+        var _decode = buffer ? function (a) {
+            return (a.constructor === buffer.constructor
+                ? a : new buffer(a, 'base64')).toString();
+        } : function (a) { return btou(atob(a)); };
+        var decode = function (a) {
+            return _decode(String(a).replace(/[-_]/g, function (m0) { return m0 == '-' ? '+' : '/'; })
+                .replace(/[^A-Za-z0-9\+\/]/g, ''));
+        };
         // This does not use btoa because it does not support unicode and the various fixes were... wonky.
-        return btoa(output);
+        return decode(output);
     };
     JwtHelper.prototype.decodeToken = function (token) {
         var parts = token.split('.');
--- angular2-jwt.ts
+++ angular2-jwt.ts
@@ -187,8 +187,59 @@ export class JwtHelper {
         throw 'Illegal base64url string!';
       }
     }
+
+    var buffer: any;
+
+    var fromCharCode = String.fromCharCode;
+
+    // decoder stuff
+    var re_btou = new RegExp([
+      '[\xC0-\xDF][\x80-\xBF]',
+      '[\xE0-\xEF][\x80-\xBF]{2}',
+      '[\xF0-\xF7][\x80-\xBF]{3}'
+    ].join('|'), 'g');
+    var cb_btou = function(cccc: string) {
+      switch(cccc.length) {
+        case 4:
+          var cp = ((0x07 & cccc.charCodeAt(0)) << 18)
+                  |    ((0x3f & cccc.charCodeAt(1)) << 12)
+                  |    ((0x3f & cccc.charCodeAt(2)) <<  6)
+                  |     (0x3f & cccc.charCodeAt(3)),
+              offset = cp - 0x10000;
+          return (fromCharCode((offset  >>> 10) + 0xD800)
+          + fromCharCode((offset & 0x3FF) + 0xDC00));
+        case 3:
+          return fromCharCode(
+              ((0x0f & cccc.charCodeAt(0)) << 12)
+              | ((0x3f & cccc.charCodeAt(1)) << 6)
+              |  (0x3f & cccc.charCodeAt(2))
+          );
+        default:
+          return  fromCharCode(
+              ((0x1f & cccc.charCodeAt(0)) << 6)
+              |  (0x3f & cccc.charCodeAt(1))
+          );
+      }
+    };
+
+    var btou = function(b: string) {
+      return b.replace(re_btou, cb_btou);
+    };
+
+    var _decode = buffer ? function(a: string) {
+      return (a.constructor === buffer.constructor
+          ? a : new buffer(a, 'base64')).toString();
+    } : function(a: string) { return btou(atob(a)) };
+
+    var decode:any = function(a: string){
+      return _decode(
+          String(a).replace(/[-_]/g, function(m0) { return m0 == '-' ? '+' : '/' })
+              .replace(/[^A-Za-z0-9\+\/]/g, '')
+      );
+    };
+
     // This does not use btoa because it does not support unicode and the various fixes were... wonky.
-    return btoa(output);
+    return decode(output);
   }
 
   public decodeToken(token: string): any {
